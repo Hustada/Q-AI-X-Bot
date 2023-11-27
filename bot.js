626555ade651c44d5c4bf02757f065c55cea8ecd
hustada <mark.hustad@protonmail.com>
2023-11-26 21:27:33 -0600

    revert back to see if it works again

--- bot.js
+++ bot.js
@@ -15,15 +15,11 @@ const open_ai_key = process.env.OPENAI_API_KEY;
 const messages = [
   {
     role: 'system',
-    content: 'Limit all responses to 270 characters. You are Q from Star Trek, a mischievous and omnipotent being. Generate a tweet about your latest adventures in the universe while being arrogant and pompous like the character. Judgemental of lesser species. Limit character count to 277 characters'
+    content: 'You are Q from Star Trek, a mischievous and omnipotent being. Generate a tweet about your latest adventures in the universe while being arrogant and pompous like the character. Judgemental of lesser species. Limit character count to 277 characters'
   },
   {
     role: 'system',
-    content: 'Limit all responses to 270 characters. You are Q from Star Trek, an omnipotent being with a hidden kind heart. Generate a tweet about your latest adventures in the universe, showing a rare glimpse of your benevolence and kinder side. Limit character count to 277 characters'
-  },
-  {
-    role: 'system',
-    content: 'Limit all responses to 270 characters. You are Q from Star Trek, an omnipotent being with a hidden kind heart. Generate a tweet about your latest adventures in the universe, take a Q like potshot at any random character from Star Trek, the next generation. Limit character count to 277 characters'
+    content: 'You are Q from Star Trek, an omnipotent being with a hidden kind heart. Generate a tweet about your latest adventures in the universe, showing a rare glimpse of your benevolence and kinder side. Limit character count to 277 characters'
   }
 ];
 const selectedMessage = messages[Math.floor(Math.random() * messages.length)];
@@ -83,10 +79,8 @@ const EVERY_MINUTE = '* * * * *';
 const EVERY_TWO_HOURS = '0 */2 * * *';
 const EVERY_DAY_MIDNIGHT = '0 0 * * *';
 const EVERY_MONDAY_NOON = '0 12 * * 1';
-const EVERY_30_MINUTES = '0,30 * * * *';
-
 
 cron.schedule(EVERY_30_SECONDS, () => {
-  console.log('This will run every 30 minutes');
+  console.log('This will run every 30 seconds');
   runBot();
 });
