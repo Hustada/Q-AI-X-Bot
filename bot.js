d14543bb9a4d955d62ba337ce3a3cf7c54344645
hustada <mark.hustad@protonmail.com>
2023-11-28 06:02:10 -0600

    change runtimes

--- bot.js
+++ bot.js
@@ -92,7 +92,7 @@ const EVERY_DAY_MIDNIGHT = '0 0 * * *';
 const EVERY_MONDAY_NOON = '0 12 * * 1';
 const EVERY_30_MINUTES = '0,30 * * * *';
 
-cron.schedule(EVERY_30_MINUTES, () => {
-  console.log('This will run every 30 minutes');
+cron.schedule(EVERY_MINUTE, () => {
+  console.log('This will run every minute');
   runBot();
 });
